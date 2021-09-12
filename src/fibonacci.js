48bb2f7776117741624ed555c3a4412aef7ab604
William Castellano <williamc246@gmail.com>
2021-09-12 11:26:18 -0700

    added fibonacci sequence

---
+++ src/fibonacci.js
@@ -0,0 +1,23 @@
+function fibonacci(max) {
+    const FIBONACCI_ZERO = 0;
+    const FIBONACCI_ONE = 1;
+    let result;
+    let i;
+    let nMinusOne = FIBONACCI_ZERO;
+    let nMinusTwo = FIBONACCI_ONE;
+    for (i = 0; i < max; i++) {
+      result = nMinusOne + nMinusTwo;
+      nMinusTwo = nMinusOne;
+      nMinusOne = result;
+      writeItOut(result);
+    }
+  }
+  
+  function writeItOut(data) {
+    console.log(data);
+    document.getElementById("fibonacci").innerHTML += `${data} `;
+  }
+  
+  fibonacci(20);
+  // TODO
+  // refactor to get rid of the "for" loop
