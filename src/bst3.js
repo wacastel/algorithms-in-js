5336ed926b799ad09a3cd57d8dc464536a345aaa
William Castellano <williamc246@gmail.com>
2025-05-09 14:55:24 -0700

    added bst3.js

---
+++ src/bst3.js
@@ -0,0 +1,39 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function levelOrderTraversal(root) {
+    if (!root) {
+        return [];
+    }
+    let queue = [root];
+    let result = [];
+    while (queue.length > 0) {
+        let levelSize = queue.length;
+        let currentLevel = [];
+        for (let i = 0; i < levelSize; i++) {
+            let currentNode = queue.shift();
+            currentLevel.push(currentNode.data);
+            if (currentNode.left) {
+                queue.push(currentNode.left);
+            }
+            if (currentNode.right) {
+                queue.push(currentNode.right);
+            }
+        }
+        result.push(currentLevel);
+    }
+    return result;
+}
+let root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+root.left.left = new Node(4);
+root.left.right = new Node(5);
+root.right.right = new Node(6);
+let result = levelOrderTraversal(root);
+console.log(result);
